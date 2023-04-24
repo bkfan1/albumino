cc0997209abf8e21d951b0d70dc164231cf20197
bkfan1 <jpf8296@gmail.com>
2023-04-23 20:54:51 -0400

    use env variables

--- src/utils/firebase-app/index.js
+++ src/utils/firebase-app/index.js
@@ -1,17 +1,13 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import {getStorage} from "firebase/storage"
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAGodV6Aswm8pAFEitquTIWZl-yTaxp2Tk",
-  authDomain: "albumino-2e5c7.firebaseapp.com",
-  projectId: "albumino-2e5c7",
-  storageBucket: "albumino-2e5c7.appspot.com",
-  messagingSenderId: "612643855230",
-  appId: "1:612643855230:web:e368cb368865811fed312f"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
 };
 
 // Initialize Firebase
