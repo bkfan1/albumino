161094589b9f419fbb527f29b3394458f5a34154
bkfan1 <jpf8296@gmail.com>
2023-05-06 17:09:12 -0400

    Added logic to add  and remove the current photo from the visor to available user albums

---
+++ src/components/AvailableAlbumCard.js
@@ -0,0 +1,88 @@
+import { PhotoVisorContext } from "@/contexts/PhotoVisorContext";
+import { Box, HStack, Icon, Text, VStack } from "@chakra-ui/react";
+import { useContext } from "react";
+import { MdOutlinePhotoAlbum } from "react-icons/md";
+import { useToast } from "@chakra-ui/react";
+import axios from "axios";
+
+export default function AvailableAlbumCard({ album }) {
+  const {
+    photo,
+    setShowAvailableAlbums,
+    setAvailableAlbums,
+  } = useContext(PhotoVisorContext);
+
+  const isPhotoAlreadyOnAlbum =
+    photo.albums.includes(album.id) ||
+    album.photos.find((albumPhoto) => albumPhoto.id === photo.id)
+      ? true
+      : false;
+
+  const toast = useToast();
+
+  const handleAddToAlbum = async (albumId) => {
+    try {
+      const data = { albums: [...photo.albums, { _id: albumId }] };
+      const res = await axios.put(`/api/photo/${photo.id}`, data);
+      toast({
+        status: "success",
+        title: "Added Photo to album.",
+        duration: 5000,
+        isClosable: false,
+      });
+      setShowAvailableAlbums(false);
+      setAvailableAlbums([]);
+    } catch (error) {
+      console.log(error)
+      toast({
+        status: "error",
+        title: "Error",
+        description:
+          "An error occurred while attempting to add photo to album.",
+        duration: 5000,
+        isClosable: false,
+      });
+    }
+  };
+
+  return (
+    <>
+      <HStack
+        key={album.id}
+        onClick={() =>
+          isPhotoAlreadyOnAlbum ? null : handleAddToAlbum(album.id)
+        }
+        _hover={{
+          cursor: isPhotoAlreadyOnAlbum ? "" : "pointer",
+          backgroundColor: !isPhotoAlreadyOnAlbum && "whitesmoke",
+        }}
+        borderRadius={"md"}
+        width={"100%"}
+        opacity={isPhotoAlreadyOnAlbum && "75%"}
+        backgroundColor={isPhotoAlreadyOnAlbum ? "whitesmoke" : ""}
+      >
+        <Box
+          width={"80px"}
+          height={"80px"}
+          backgroundColor={"lightgray"}
+          borderRadius={"md"}
+          as="figure"
+          display={"flex"}
+          flexDirection={"column"}
+          alignItems={"center"}
+          justifyContent={"center"}
+        >
+          <MdOutlinePhotoAlbum />
+        </Box>
+        <VStack>
+          <Text width={"100%"}>{album.name}</Text>
+          {photo.albums.includes(album.id) || isPhotoAlreadyOnAlbum ? (
+            <Text fontStyle={"italic"}>Already on this album</Text>
+          ) : (
+            ""
+          )}
+        </VStack>
+      </HStack>
+    </>
+  );
+}
