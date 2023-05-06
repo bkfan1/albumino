9d859591d45f7ce55875e60283dba64e6e8c04c0
bkfan1 <jpf8296@gmail.com>
2023-05-06 17:19:59 -0400

    Added conditional rendering for unique album route

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -9,7 +9,7 @@ export default function Layout({ children }) {
   return (
     <Flex flexDirection={"column"} minHeight={"100vh"}>
       <Navbar />
-      <Flex flexDirection={"row"} width={"100%"} height={"100%"}>
+      <Flex flexDirection={"row"} width={"100%"} height={"100%"} gap={4} paddingY={4}>
         {pathname === "/album/[albumId]" ? "" : <Panel />}
         {children}
       </Flex>
