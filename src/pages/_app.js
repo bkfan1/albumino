7f44326d8f223abd90b740be4c24ae12b1bed1fb
bkfan1 <jpf8296@gmail.com>
2023-05-06 17:25:49 -0400

    Wrapped 'Component' with SessionProvider

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,16 @@
 import "@/styles/globals.css";
 import { ChakraProvider } from "@chakra-ui/react";
+import { SessionProvider } from "next-auth/react";
 
-function MyApp({ Component, pageProps }) {
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}) {
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <SessionProvider session={session}>
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </SessionProvider>
   );
 }
-
-export default MyApp;
