86967819d0097dde94370a07f23ed24ba1e11660
bkfan1 <jpf8296@gmail.com>
2023-05-06 17:13:00 -0400

    Implemented logic to upload many photos from a single request

--- src/pages/photos/index.js
+++ src/pages/photos/index.js
@@ -1,7 +1,44 @@
-export default function PhotosPage(){
-    return(
-        <>
-        <h1>Photos Page</h1>
-        </>
-    )
+import Layout from "@/components/Layout";
+import PhotosGrid from "@/components/PhotosGrid";
+import { Flex } from "@chakra-ui/react";
+import { getServerSession } from "next-auth";
+import connection from "@/database/connection";
+import Photo from "@/database/models/photo";
+import { authOptions } from "../api/auth/[...nextauth]";
+
+export default function PhotosPage({ photos }) {
+  return (
+    <>
+      <Layout>
+        <Flex flex={6} paddingRight={4}>
+          <PhotosGrid photos={photos}></PhotosGrid>
+        </Flex>
+      </Layout>
+    </>
+  );
+}
+
+export async function getServerSideProps({ req, res }) {
+  const session = await getServerSession(req, res, authOptions);
+
+  const db = await connection();
+
+  const rawPhotos = await Photo.find({
+    author_account_id: session.user.accountId,
+  }).sort({ uploaded_at: "desc" });
+
+  const photos = [];
+  for (const rawPhoto of rawPhotos) {
+    photos.push({
+      id: rawPhoto._id.toString(),
+      albums: rawPhoto.albums.map((albumId) => albumId.toString()),
+      url: rawPhoto.url,
+    });
+  }
+
+  return {
+    props: {
+      photos,
+    },
+  };
 }
