44ad81c6d8e080c9c0cb1ace1d4a23eda65e250a
bkfan1 <jpf8296@gmail.com>
2023-05-08 18:23:51 -0400

    Added conditional rendering for buttons styling when is in a specific route

--- src/components/Panel.js
+++ src/components/Panel.js
@@ -9,6 +9,7 @@ import {
 } from "@chakra-ui/react";
 import { nanoid } from "nanoid";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import { AiOutlineSearch } from "react-icons/ai";
 import { HiOutlinePhotograph } from "react-icons/hi";
 import { MdOutlinePeopleAlt, MdOutlinePhotoAlbum } from "react-icons/md";
@@ -29,13 +30,17 @@ const linkButtons = [
 ];
 
 export default function Panel() {
+
+  const router = useRouter();
+  const {pathname} = router;
+
   return (
     <>
-      <VStack flex={1}>
-        <ButtonGroup width={"100%"} orientation="vertical" variant={"ghost"}>
+      <VStack flex={1} borderRight={"1px"} borderColor={"#edf1f5"}>
+        <ButtonGroup width={"100%"} orientation="vertical" variant={"ghost"} marginTop={2}>
           {linkButtons.map(({ id, text, href, icon }) => (
             <Link key={id} href={href}>
-              <Button leftIcon={icon} width={"100%"}>{text}</Button>
+              <Button leftIcon={icon} width={"100%"} color={href === pathname ? "#2178e8" : ""} backgroundColor={href === pathname ? "#e8f0fe" : ""} >{text}</Button>
             </Link>
           ))}
         </ButtonGroup>
