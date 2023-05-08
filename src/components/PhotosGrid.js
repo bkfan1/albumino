4cf1bdaa1042543c117d978cfaf7c6868c87360b
bkfan1 <jpf8296@gmail.com>
2023-05-07 21:14:42 -0400

    Wrapper PhotosGridItem component inside PhotoVisorProvider component

--- src/components/PhotosGrid.js
+++ src/components/PhotosGrid.js
@@ -1,14 +1,21 @@
 import { Box, Grid } from "@chakra-ui/react";
 import PhotosGridItem from "./PhotosGridItem";
+import { PhotoVisorProvider } from "@/contexts/PhotoVisorContext";
 
 export default function PhotosGrid({ photos }) {
   return (
+    <>
     <Box width={"100%"}>
-      <Grid templateColumns="repeat(auto-fill, minmax(200px, 1fr))" gap={4}>
+      <Grid templateColumns="repeat(auto-fit, minmax(200px, 1fr))" gap={4}>
+      
+      <PhotoVisorProvider photos={photos}>
         {photos.map((photo) => (
           <PhotosGridItem key={photo.id} data={photo} />
         ))}
+        </PhotoVisorProvider>
       </Grid>
+    
     </Box>
+    </>
   );
 }
