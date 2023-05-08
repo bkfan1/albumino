17ffd9a27298d5adcfac120bc6594ed10e4d94cc
bkfan1 <jpf8296@gmail.com>
2023-05-08 18:27:36 -0400

    Added middleware to handle DELETE requests

--- src/pages/api/album/[albumId].js
+++ src/pages/api/album/[albumId].js
@@ -1,28 +1,10 @@
-import connection from "@/database/connection";
-import Album from "@/database/models/album";
-import Photo from "@/database/models/photo";
+import { deleteAlbum } from "@/middlewares/album";
 
 export default async function handler(req, res) {
+
   switch (req.method) {
     case "DELETE":
-      try {
-        const db = await connection();
-
-        const updatedPhotos = await Photo.updateMany(
-          {
-            albums: req.query.albumId,
-          },
-          { $pull: { albums: req.query.albumId } }
-        );
-        const deletedAlbum = await Album.findByIdAndDelete({
-          _id: req.query.albumId,
-        });
-
-        return res.status(200).json({});
-      } catch (error) {
-        return res.status(500).json({});
-      }
-
+      return await deleteAlbum(req, res);
       break;
 
     default:
