7367f773c6c2ba7ae6576e1f5a6b507c36895d1f
bkfan1 <jpf8296@gmail.com>
2023-05-07 21:15:42 -0400

    Added logic and states to handle the PhotoVisor functionality

--- src/contexts/PhotoVisorContext.js
+++ src/contexts/PhotoVisorContext.js
@@ -1,16 +1,41 @@
+import { useDisclosure } from "@chakra-ui/react";
 import axios from "axios";
 import { createContext, useState } from "react";
 
 export const PhotoVisorContext = createContext();
 
-export const PhotoVisorProvider = ({ children, photo, modal}) => {
+export const PhotoVisorProvider = ({children, photos}) => {
 
-  const [showAvailableAlbums, setShowAvailableAlbums] = useState(false);
-  const [availableAlbums, setAvailableAlbums] =useState([])
+  const {isOpen, onClose, onOpen} = useDisclosure();
+
+  const [currentPhoto, setCurrentPhoto] = useState();
+
+  const [showAvailableAlbums, setShowAvailableAlbums] = useState(false)
+  const [availableAlbums, setAvailableAlbums] = useState([]);
+
+  const handleSetNextPhoto = ()=>{
+
+    const currentPhotoIndex = photos.findIndex((photo)=>photo.id === currentPhoto.id);
+
+    if(!(currentPhotoIndex === photos.length - 1)){
+      setCurrentPhoto(photos[currentPhotoIndex + 1])
+      return
+    }
+  }
+
+  const handleSetPreviousPhoto = ()=>{
+    const currentPhotoIndex = photos.findIndex((photo)=>photo.id === currentPhoto.id);
+    if((currentPhotoIndex > 0)){
+      setCurrentPhoto(photos[currentPhotoIndex-1]);
+      return;
+    }
+
+
+  }
 
   return (
     <>
-      <PhotoVisorContext.Provider value={{photo, modal, showAvailableAlbums, setShowAvailableAlbums, availableAlbums, setAvailableAlbums}}>
+      <PhotoVisorContext.Provider value={{photos, isOpen, onClose, onOpen, currentPhoto, setCurrentPhoto, showAvailableAlbums, setShowAvailableAlbums, availableAlbums, setAvailableAlbums, handleSetNextPhoto, handleSetPreviousPhoto}} >
         {children}
       </PhotoVisorContext.Provider>
     </>
