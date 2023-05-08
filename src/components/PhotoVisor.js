a8cec6a06bac6c33023a9523a635fb954159ed4a
bkfan1 <jpf8296@gmail.com>
2023-05-07 21:09:14 -0400

    Consumed onOpen from PhotoVisorContext

--- src/components/PhotoVisor.js
+++ src/components/PhotoVisor.js
@@ -1,39 +1,18 @@
-import { useContext, useState } from "react";
-import {
-  Box,
-  ButtonGroup,
-  Flex,
-  Button,
-  HStack,
-  IconButton,
-  Image,
-  Menu,
-  MenuButton,
-  MenuItem,
-  MenuList,
-  Modal,
-  ModalOverlay,
-  Text,
-  VStack,
-  useDisclosure,
-  useToast,
-} from "@chakra-ui/react";
-import { BsInfoCircle, BsThreeDots, BsTrash, BsX } from "react-icons/bs";
-import { useRouter } from "next/router";
+import { useContext } from "react";
+import { Modal, ModalOverlay } from "@chakra-ui/react";
 import PhotoVisorHeader from "./PhotoVisorHeader";
 import PhotoVisorBody from "./PhotoVisorBody";
 import { PhotoVisorContext } from "@/contexts/PhotoVisorContext";
 
-export default function PhotoVisor({ }) {
-
-  const {isOpen, onClose} = useContext(PhotoVisorContext);
+export default function PhotoVisor({}) {
+  const { isOpen } = useContext(PhotoVisorContext);
 
   return (
     <>
       <Modal isOpen={isOpen} blockScrollOnMount={true}>
         <ModalOverlay>
-          <PhotoVisorHeader/>
-          <PhotoVisorBody/>
+          <PhotoVisorHeader />
+          <PhotoVisorBody />
         </ModalOverlay>
       </Modal>
     </>
