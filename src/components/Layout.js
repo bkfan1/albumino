b484ad986591a4b0f0a793a89975d341ce193165
bkfan1 <jpf8296@gmail.com>
2023-05-08 18:31:18 -0400

    Removed unused imports

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,15 +1,15 @@
-import { Container, Flex } from "@chakra-ui/react";
+import { Flex } from "@chakra-ui/react";
 import Navbar from "./Navbar";
 import Panel from "./Panel";
 import { useRouter } from "next/router";
 
 export default function Layout({ children }) {
-  const router = useRouter()
-  const {pathname} = router;
+  const router = useRouter();
+  const { pathname } = router;
   return (
     <Flex flexDirection={"column"} minHeight={"100vh"}>
       <Navbar />
-      <Flex flexDirection={"row"} width={"100%"} height={"100%"} gap={4} paddingY={4}>
+      <Flex flexDirection={"row"} width={"100%"} height={"100%"} gap={4}>
         {pathname === "/album/[albumId]" ? "" : <Panel />}
         {children}
       </Flex>
