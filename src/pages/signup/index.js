a4755954a33c482d68e5439688d064f65723b1b0
bkfan1 <jpf8296@gmail.com>
2023-05-10 18:50:22 -0400

    Added check to see if user is logged

--- src/pages/signup/index.js
+++ src/pages/signup/index.js
@@ -1,9 +1,28 @@
 import SignUpForm from "@/components/SignUpForm";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../api/auth/[...nextauth]";
 
-export default function SignUpPage(){
-    return(
-        <>
-        <SignUpForm/>
-        </>
-    )
+export default function SignUpPage() {
+  return (
+    <>
+      <SignUpForm />
+    </>
+  );
+}
+
+export async function getServerSideProps({req, res}) {
+  const session = await getServerSession(req, res, authOptions);
+
+  if (session) {
+    return {
+      redirect: {
+        destination: "/photos",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
 }
