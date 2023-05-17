e929bc3ee6ad4a65aacc3a05b0b1cdbb55be2691
bkfan1 <jpf8296@gmail.com>
2023-05-17 18:25:08 -0400

    Wrapped component inside a Skeleton

--- src/components/PhotosGridItem.js
+++ src/components/PhotosGridItem.js
@@ -1,11 +1,22 @@
 import { PhotoVisorContext } from "@/contexts/PhotoVisorContext";
-import { Box, Checkbox, GridItem, Image, Tooltip } from "@chakra-ui/react";
-import { useContext } from "react";
+import {
+  Box,
+  Checkbox,
+  GridItem,
+  Image,
+  Skeleton,
+  Tooltip,
+} from "@chakra-ui/react";
+import { useContext, useEffect, useState } from "react";
 import PhotoVisor from "./PhotoVisor";
+import { useIsMounted } from "@/hooks/useIsMounted";
 
 export default function PhotosGridItem({ data }) {
   const { onOpen, setCurrentPhoto } = useContext(PhotoVisorContext);
   const { url } = data;
+
+  const { isMounted } = useIsMounted();
+
   return (
     <>
       <GridItem>
@@ -14,24 +25,26 @@ export default function PhotosGridItem({ data }) {
           overflow={"hidden"}
           _hover={{
             opacity: "90%",
-            transition: "ease-in-out all",
+            transition: "100ms ease-in-out",
             cursor: "pointer",
           }}
         >
-          <Tooltip label="Select this photo">
-            <Checkbox position={"absolute"} size={"md"} margin={2} />
-          </Tooltip>
-          <Image
-            src={url}
-            alt={"Photo"}
-            w="100%"
-            h="auto"
-            borderRadius="md"
-            onClick={() => {
-              setCurrentPhoto(data);
-              onOpen();
-            }}
-          />
+          <Skeleton isLoaded={isMounted} rounded={"md"}>
+            <Tooltip label="Select this photo">
+              <Checkbox position={"absolute"} size={"md"} margin={2} />
+            </Tooltip>
+            <Image
+              src={url}
+              alt={"Photo"}
+              w="100%"
+              h="auto"
+              rounded="md"
+              onClick={() => {
+                setCurrentPhoto(data);
+                onOpen();
+              }}
+            />
+          </Skeleton>
         </Box>
       </GridItem>
       <PhotoVisor />
