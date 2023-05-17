3cbda91325b94fd34f6db6fefedaa463a70338ba
bkfan1 <jpf8296@gmail.com>
2023-05-17 18:22:27 -0400

    Wrapped many components inside Tooltip

--- src/components/Panel.js
+++ src/components/Panel.js
@@ -8,6 +8,7 @@ import {
   Icon,
   IconButton,
   Progress,
+  Skeleton,
   Text,
   Tooltip,
   VStack,
@@ -15,11 +16,12 @@ import {
 import { nanoid } from "nanoid";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { useEffect } from "react";
 import { BsCloud } from "react-icons/bs";
 import { HiOutlinePhotograph } from "react-icons/hi";
-import {
-  MdOutlinePhotoAlbum,
-} from "react-icons/md";
+import { MdOutlinePhotoAlbum } from "react-icons/md";
+import { useState } from "react";
+import { useIsMounted } from "@/hooks/useIsMounted";
 
 const linkButtons = [
   {
@@ -39,6 +41,7 @@ const linkButtons = [
 export default function Panel() {
   const router = useRouter();
   const { pathname } = router;
+  const { isMounted } = useIsMounted();
 
   return (
     <>
@@ -50,6 +53,7 @@ export default function Panel() {
         backgroundColor={"white"}
         className="panel"
         gap={2}
+        paddingX={1}
       >
         <ButtonGroup
           orientation="vertical"
@@ -57,51 +61,68 @@ export default function Panel() {
           marginTop={2}
         >
           {linkButtons.map(({ id, href, text, icon }) => (
-            <Tooltip key={id} label={text}>
-              <Link href={href}>
-                <Button
-                  display={{ sm: "none", md: "initial" }}
-                  width={"100%"}
-                  variant={"ghost"}
-                  leftIcon={icon}
-                >
-                  {text}
-                </Button>
-                <IconButton
-                  display={{ base: "none", sm: "flex", md: "none" }}
-                  icon={icon}
-                  rounded={"full"}
-                  variant={"ghost"}
-                  size={{ base: "lg" }}
-                />
-              </Link>
-            </Tooltip>
+            <Skeleton key={id} isLoaded={isMounted} rounded={"md"}>
+              <Tooltip label={text}>
+                <Link href={href}>
+                  <Button
+                    display={{ sm: "none", md: "initial" }}
+                    width={"100%"}
+                    variant={"ghost"}
+                    leftIcon={icon}
+                  >
+                    {text}
+                  </Button>
+                  <IconButton
+                    display={{ base: "none", sm: "flex", md: "none" }}
+                    icon={icon}
+                    rounded={"full"}
+                    variant={"ghost"}
+                    size={{ base: "lg" }}
+                  />
+                </Link>
+              </Tooltip>
+            </Skeleton>
           ))}
         </ButtonGroup>
         <Divider />
 
-        <VStack paddingX={2}>
-          <HStack>
-            <Icon as={BsCloud} />
+        <Skeleton isLoaded={isMounted} rounded={"md"}>
+          <VStack paddingX={2}>
+            <Tooltip label="Storaged used" display={{ sm: "flex", md: "none" }}>
+              <VStack>
+                <HStack>
+                  <Icon as={BsCloud} />
 
-            <Heading size={"sm"} display={{ sm: "none", md: "flex" }}>
-              Storage
-            </Heading>
-          </HStack>
+                  <Heading size={"sm"} display={{ sm: "none", md: "flex" }}>
+                    Storage
+                  </Heading>
+                </HStack>
 
-          <Box width={"100%"}>
-            <Progress value={50} colorScheme={"blue"} rounded={"full"} display={{sm:"none", md:"flex"}} />
-            <Heading size={"xs"} display={{sm:"flex",md:"none"}}>0%</Heading>
-          </Box>
+                <Box width={"100%"}>
+                  <Tooltip label="Storage used">
+                    <Progress
+                      value={50}
+                      colorScheme={"blue"}
+                      rounded={"full"}
+                      display={{ sm: "none", md: "flex" }}
+                    />
+                  </Tooltip>
+                  <Heading size={"xs"} display={{ sm: "flex", md: "none" }}>
+                    0%
+                  </Heading>
+                </Box>
+              </VStack>
+            </Tooltip>
 
-          <Text
-            textAlign={"center"}
-            display={{ sm: "none", md: "initial" }}
-            fontSize={"sm"}
-          >
-            0.0 GB of 0.0 GB used
-          </Text>
-        </VStack>
+            <Text
+              textAlign={"center"}
+              display={{ sm: "none", md: "initial" }}
+              fontSize={"sm"}
+            >
+              0.0 GB of 0.0 GB used
+            </Text>
+          </VStack>
+        </Skeleton>
       </VStack>
     </>
   );
