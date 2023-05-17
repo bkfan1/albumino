7882f8868bbb956bff1cb60a3f28e46d49d23838
bkfan1 <jpf8296@gmail.com>
2023-05-17 18:27:05 -0400

    Updated imports

--- src/pages/signup/index.js
+++ src/pages/signup/index.js
@@ -1,4 +1,4 @@
-import SignUpForm from "@/components/SignUpForm";
+import SignUpForm from "@/components/forms/SignUpForm";
 import { getServerSession } from "next-auth";
 import { authOptions } from "../api/auth/[...nextauth]";
 
