f5427810c703e5dc4fc560ed8580dbf77e37b598
bkfan1 <jpf8296@gmail.com>
2023-05-17 18:27:12 -0400

    Updated imports

--- src/pages/signin/index.js
+++ src/pages/signin/index.js
@@ -1,4 +1,4 @@
-import SignInForm from "@/components/SignInForm";
+import SignInForm from "@/components/forms/SignInForm";
 import { authOptions } from "../api/auth/[...nextauth]";
 import { getServerSession } from "next-auth";
 
