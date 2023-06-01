c246078ef5c0fcca0455e7203ea2384424fd7953
bkfan1 <jpf8296@gmail.com>
2023-05-31 20:30:34 -0400

    Created model for album invitations

---
+++ src/database/models/AlbumInvitation/index.js
@@ -0,0 +1,30 @@
+
+import {Schema, models, model} from "mongoose";
+
+const AlbumInvitationSchema = new Schema({
+
+    album_id: {
+        type: Schema.ObjectId,
+        ref: "Albums",
+        required: true,
+    },
+
+    sender_id: {
+        type: Schema.ObjectId,
+        ref: "Accounts",
+        required:true,
+    },
+
+    status: {
+        type:String,
+        enum: ["pending", "accepted"],
+        default: "pending", 
+    },
+
+    created_at: {
+        type:Date,
+    }
+
+}, {collection:"albumInvitations"})
+
+export default models.AlbumInvitation || model("AlbumInvitation", AlbumInvitationSchema)
