47f34b40834dace0be7e0aee033767945b02f1d8
bkfan1 <jpf8296@gmail.com>
2023-06-04 15:27:17 -0400

    Added functions to handle invitations

--- src/pages/api/album/[albumId]/invitation/[invitationId]/index.js
+++ src/pages/api/album/[albumId]/invitation/[invitationId]/index.js
@@ -1,23 +1,49 @@
 import connection from "@/database/connection";
 import AlbumInvitation from "@/database/models/AlbumInvitation";
+import Album from "@/database/models/album";
+import { isAlbumOwner } from "@/middlewares/album";
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
+import { getServerSession } from "next-auth";
+
+const updateAlbumInvitation = async (req, res) => {
+  try {
+    const session = await getServerSession(req, res, authOptions);
+    const isOwner = await isAlbumOwner(
+      req.query.albumId,
+      session.user.accountId
+    );
+
+    if (isOwner) {
+      return res.status(400).json({});
+    }
+
+    const db = await connection();
+
+    const updatedInvitation = await AlbumInvitation.findByIdAndUpdate(
+      { _id: req.query.invitationId },
+      {
+        status: req.body.status,
+      }
+    );
+
+    if (req.body.status === "accepted") {
+      await Album.findByIdAndUpdate(
+        { _id: updated.album_id },
+        { $push: { contributors: session.user.accountId } }
+      );
+    }
+
+    return res.status(200).json({});
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({});
+  }
+};
 
 export default async function handler(req, res) {
   switch (req.method) {
     case "PUT":
-      try {
-        const db = await connection();
-        const updated = await AlbumInvitation.findByIdAndUpdate(
-          { _id: req.query.invitationId },
-          {
-            status: req.body.status,
-          }
-        );
-
-        return res.status(200).json({})
-      } catch (error) {
-        return res.status(500).json({});
-      }
-
+      return await updateAlbumInvitation(req, res);
       break;
 
     default:
--- src/pages/api/album/[albumId]/invitation/index.js
+++ src/pages/api/album/[albumId]/invitation/index.js
@@ -1,51 +1,71 @@
 import connection from "@/database/connection";
 import AlbumInvitation from "@/database/models/AlbumInvitation";
+import { isAlbumOwner } from "@/middlewares/album";
 import { authOptions } from "@/pages/api/auth/[...nextauth]";
 import { getServerSession } from "next-auth";
 import { createTransport } from "nodemailer";
 
+const sendEmail = async (data) => {
+  try {
+    const transporter = createTransport({
+      service: "gmail",
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false,
+
+      auth: {
+        user: process.env.NODEMAILER_EMAIL,
+        pass: process.env.NODEMAILER_APP_PASSWORD,
+      },
+    });
+
+    const info = await transporter.sendMail({
+      from: process.env.NODEMAILER_EMAIL,
+      to: data.email,
+
+      subject: data.subject,
+
+      text: data.text,
+    });
+
+    return true;
+  } catch (error) {
+    throw Error("");
+  }
+};
+
+const createAlbumInvitation = async (req, res) => {
+  try {
+    const session = await getServerSession(req, res, authOptions);
+    const isOwner = await isAlbumOwner(req.query.albumId, session.user.accountId);
+
+    if(!isOwner){return res.status(403).json({})}
+
+    const db = await connection();
+
+    const createdInvitation = await AlbumInvitation.create({
+      album_id: req.query.albumId,
+
+      sender_id: session.user.accountId,
+
+      status: "pending",
+
+      created_at: new Date(),
+    });
+
+    const sentInvitationEmail = await sendEmail(req.body);
+
+    return res.status(200).json({});
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({});
+  }
+};
+
 export default async function handler(req, res) {
   switch (req.method) {
     case "POST":
-      try {
-        const session = await getServerSession(req, res, authOptions);
-        const db = await connection();
-        const invitation = await AlbumInvitation.create({
-          album_id: req.query.albumId,
-
-          sender_id: session.user.accountId,
-
-          status: "pending",
-
-          created_at: new Date(),
-        });
-
-        const transporter = createTransport({
-          service: "gmail",
-          host: "smtp.gmail.com",
-          port: 587,
-          secure: false,
-
-          auth: {
-            user: process.env.NODEMAILER_EMAIL,
-            pass: process.env.NODEMAILER_APP_PASSWORD,
-          },
-        });
-
-        const info = await transporter.sendMail({
-          from: process.env.NODEMAILER_EMAIL,
-          to: req.body.email,
-
-          subject: req.body.subject,
-
-          text: `Someone invited you to album`,
-        });
-
-        return res.status(200).json({});
-      } catch (error) {
-        console.log(error);
-        return res.status(500).json({});
-      }
+      return await createAlbumInvitation(req, res);
 
       break;
 
