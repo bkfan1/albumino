ed74ce3c2f5456a541310243e7736f8ecc61e026
bkfan1 <jpf8296@gmail.com>
2023-06-07 11:48:41 -0400

    Added endpoints to handle multiple parts of an album

--- src/pages/api/album/[albumId]/contributors/[contributorId]/index.js
+++ src/pages/api/album/[albumId]/contributors/[contributorId]/index.js
@@ -1,14 +1,11 @@
+export default function handler(req, res) {
+  switch (req.method) {
+    case "POST":
+      break;
+    case "DELETE":
+      break;
 
-
-export default function handler(req, res){
-    switch (req.method) {
-        case "POST":
-            
-            break;
-        case "DELETE":
-            break;
-    
-        default:
-            break;
-    }
+    default:
+      break;
+  }
 }
--- src/pages/api/album/[albumId]/invitation/[invitationId]/index.js
+++ src/pages/api/album/[albumId]/invitation/[invitationId]/index.js
@@ -1,44 +1,4 @@
-import connection from "@/database/connection";
-import AlbumInvitation from "@/database/models/AlbumInvitation";
-import Album from "@/database/models/album";
-import { isAlbumOwner } from "@/middlewares/album";
-import { authOptions } from "@/pages/api/auth/[...nextauth]";
-import { getServerSession } from "next-auth";
-
-const updateAlbumInvitation = async (req, res) => {
-  try {
-    const session = await getServerSession(req, res, authOptions);
-    const isOwner = await isAlbumOwner(
-      req.query.albumId,
-      session.user.accountId
-    );
-
-    if (isOwner) {
-      return res.status(400).json({});
-    }
-
-    const db = await connection();
-
-    const updatedInvitation = await AlbumInvitation.findByIdAndUpdate(
-      { _id: req.query.invitationId },
-      {
-        status: req.body.status,
-      }
-    );
-
-    if (req.body.status === "accepted") {
-      await Album.findByIdAndUpdate(
-        { _id: updated.album_id },
-        { $push: { contributors: session.user.accountId } }
-      );
-    }
-
-    return res.status(200).json({});
-  } catch (error) {
-    console.log(error);
-    return res.status(500).json({});
-  }
-};
+import { updateAlbumInvitation } from "@/middlewares/invitation";
 
 export default async function handler(req, res) {
   switch (req.method) {
