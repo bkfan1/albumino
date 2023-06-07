3341a8a8fa00dc9769c665fe2eb171fd646d97cf
bkfan1 <jpf8296@gmail.com>
2023-06-07 11:42:25 -0400

    Improved logic inside getServerSideProps

--- src/pages/invitation/[invitationId]/index.js
+++ src/pages/invitation/[invitationId]/index.js
@@ -1,97 +1,103 @@
 import connection from "@/database/connection";
 import AlbumInvitation from "@/database/models/AlbumInvitation";
-import Account from "@/database/models/account";
-import Album from "@/database/models/album";
+import Account from "@/database/models/Account";
+import Album from "@/database/models/Album";
 import { authOptions } from "@/pages/api/auth/[...nextauth]";
-import { Button, Heading, VStack } from "@chakra-ui/react";
-import axios from "axios";
+import { Flex, Heading } from "@chakra-ui/react";
 import { getServerSession } from "next-auth";
-import { useRouter } from "next/router";
-import { useForm } from "react-hook-form";
+
+import { invitationExists, isInvitationAuthor } from "@/middlewares/invitation";
+import { isAlbumContributor } from "@/middlewares/album";
+import AlbumInvitationForm from "@/components/ui/forms/AlbumInvitationForm";
 
 export default function InvitationPage({ details }) {
-  const { invitation, author, album } = details;
-
-  const {handleSubmit} = useForm();
-  const router = useRouter()
-
-  const onSubmit = async()=>{
-    try {
-        const data = {
-            status: "accepted"
-        }
-        const res = await axios.put(`/api/album/${album.id}/invitation/${invitation.id}`, data);
-        console.log("yes!")
-        router.push(`/album/${album.id}`)
-        
-    } catch (error) {
-        console.log(error)
-        
-    }
-  }
-
-
   return (
     <>
-    {invitation.status === "pending" && <VStack as={"form"} maxWidth={"sm"} onSubmit={handleSubmit(onSubmit)}>
-            <VStack>
-              <Heading size={"lg"} textAlign={"center"}>
-                {author.name} invited you to {album.name}
-              </Heading>
-            </VStack>
-            <Button type="submit" width={"100%"} colorScheme="green">
-              Accept invitation
-            </Button>
-          </VStack>}
-    {invitation.status === "accepted" && <Heading>Invitation expired</Heading>}
-
+      <Flex
+        as="main"
+        minHeight={"100vh"}
+        justifyContent={"center"}
+        alignItems={"center"}
+      >
+        {invitation.status === "pending" && (
+          <AlbumInvitationForm details={details} />
+        )}
+        {invitation.status === "accepted" && (
+          <Heading>Invitation expired</Heading>
+        )}
+      </Flex>
     </>
   );
 }
 
 export async function getServerSideProps({ req, res, query }) {
-  const db = await connection();
-  const session = await getServerSession(req, res, authOptions);
-  const foundInvitation = await AlbumInvitation.findById(query.invitationId);
+  try {
+    const session = await getServerSession(req, res, authOptions);
 
-  if (!foundInvitation) {
+    const db = await connection();
+    const exists = await invitationExists(query.invitationId);
+
+    if (!exists) {
+      return {
+        redirect: {
+          permanent: false,
+          destination: "/404",
+        },
+      };
+    }
+
+    const foundInvitation = await AlbumInvitation.findById({
+      _id: query.invitationId,
+    });
+
+    const isAuthor = await isInvitationAuthor(
+      query.invitationId,
+      session.user.accountId
+    );
+
+    const isContributor = await isAlbumContributor(
+      albumDetails._id.toString(),
+      session.user.accountId
+    );
+
+    if (isAuthor || isContributor) {
+      return {
+        redirect: {
+          destination: "/",
+          permanent: false,
+        },
+      };
+    }
+
+    const albumDetails = await Album.findById(foundInvitation.album_id);
+
+    const author = await Account.findById(albumDetails.author_account_id);
+
+    return {
+      props: {
+        details: {
+          author: {
+            name: `${author.firstname} ${author.lastname}`,
+          },
+
+          album: {
+            id: albumDetails._id.toString(),
+            name: albumDetails.name,
+          },
+
+          invitation: {
+            id: foundInvitation._id.toString(),
+            status: foundInvitation.status,
+          },
+        },
+      },
+    };
+  } catch (error) {
     return {
       redirect: {
+        destination: "/500",
         permanent: false,
-        destination: "/404",
       },
     };
   }
-
-  const author = await Account.findById(foundInvitation.sender_id);
-  if(session.user.accountId === author._id.toString()){
-    return {
-        redirect: {
-            permanent: false,
-            destination:"/"
-        }
-    }
-  }
-
-  const albumDetails = await Album.findById(foundInvitation.album_id);
-
-  return {
-    props: {
-      details: {
-        author: {
-            name:`${author.firstname} ${author.lastname}`,
-        },
-
-        album: {
-            id: albumDetails._id.toString(),
-            name: albumDetails.name,
-        },
-        
-        invitation: {
-            id: foundInvitation._id.toString(),
-            status: foundInvitation.status,
-        }
-      },
-    },
-  };
 }
