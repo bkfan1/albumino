01e2f8b9692ff7a6e7eec39012289023f775e70b
bkfan1 <jpf8296@gmail.com>
2023-06-07 11:44:24 -0400

    Improved logic inside getServerSideProps

--- src/pages/albums/index.js
+++ src/pages/albums/index.js
@@ -1,5 +1,5 @@
-import AlbumCard from "@/components/AlbumCard";
-import Layout from "@/components/Layout";
+import AlbumCard from "@/components/ui/cards/AlbumCard";
+import Layout from "@/components/ui/Layout";
 import {
   Button,
   ButtonGroup,
@@ -10,15 +10,14 @@ import {
   Skeleton,
   VStack,
 } from "@chakra-ui/react";
-import { getServerSession } from "next-auth";
 import { MdOutlineAddBox } from "react-icons/md";
-import { authOptions } from "../api/auth/[...nextauth]";
-import connection from "@/database/connection";
-import Album from "@/database/models/album";
 import Link from "next/link";
 import { useIsMounted } from "@/hooks/useIsMounted";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../api/auth/[...nextauth]";
+import { getAccountAlbums } from "@/middlewares/account";
 
-export default function AlbumsPage({ ownAlbums, sharedAlbums }) {
+export default function AlbumsPage({ albums }) {
   const { isMounted } = useIsMounted();
   return (
     <>
@@ -48,18 +47,21 @@ export default function AlbumsPage({ ownAlbums, sharedAlbums }) {
 
           <Divider />
 
-          <Flex as={"section"} width={"100%"} height={"100%"} flexDirection={"column"}>
-
+          <Flex
+            as={"section"}
+            width={"100%"}
+            height={"100%"}
+            flexDirection={"column"}
+          >
             <SimpleGrid
               columns={{ sm: 2, md: 3, lg: 4, xl: 5, "2xl": 8 }}
               className="albumsGrid"
             >
-              {ownAlbums.map((album) => (
+              {albums.map((album) => (
                 <AlbumCard key={album.id} data={album} />
               ))}
             </SimpleGrid>
           </Flex>
-
         </Flex>
       </Layout>
     </>
@@ -67,25 +69,26 @@ export default function AlbumsPage({ ownAlbums, sharedAlbums }) {
 }
 
 export async function getServerSideProps({ req, res }) {
-  const session = await getServerSession(req, res, authOptions);
+  try {
+    const session = await getServerSession(req, res, authOptions);
 
-  const rawOwnAlbums = await Album.find({
-    author_account_id: session.user.accountId,
-  });
-  const rawSharedAlbums = await Album.find({
-    contributors: session.user.accountId,
-  });
+    const albums = await getAccountAlbums(session.user.accountId);
 
-  return {
-    props: {
-      ownAlbums: rawOwnAlbums.map(({ _id, name }) => ({
-        id: _id.toString(),
-        name,
-      })),
-      sharedAlbums: rawSharedAlbums.map(({ _id, name }) => ({
-        id: _id.toString(),
-        name,
-      })),
-    },
-  };
+    if (!albums) {
+      return { redirect: { destination: "/404", permanent: false } };
+    }
+
+    return {
+      props: {
+        albums,
+      },
+    };
+  } catch (error) {
+    return {
+      redirect: {
+        destination: "/500",
+        permanent: false,
+      },
+    };
+  }
 }
