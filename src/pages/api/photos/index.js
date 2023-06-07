5d758d66b7b6b2c6cc51af2e7df2d3ad32ba2ea8
bkfan1 <jpf8296@gmail.com>
2023-06-07 11:43:35 -0400

    Added comments related to api endpoint config object

--- src/pages/api/photos/index.js
+++ src/pages/api/photos/index.js
@@ -1,14 +1,6 @@
-import { getServerSession } from "next-auth";
-import { authOptions } from "../auth/[...nextauth]";
-import Photo from "@/database/models/photo";
-import connection from "@/database/connection";
-import multer, { memoryStorage } from "multer";
-import { storage } from "@/utils/firebase-app";
-import Account from "@/database/models/account";
-import { v4 } from "uuid";
-import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
 import { uploadPhotos } from "@/middlewares/photo";
 
+// Disable bodyParser to allow Multer to handle files on incoming requests
 export const config = {
   api: {
     bodyParser: false,
@@ -16,7 +8,6 @@ export const config = {
 };
 
 export default async function handler(req, res) {
-
   switch (req.method) {
     case "POST":
       return await uploadPhotos(req, res);
