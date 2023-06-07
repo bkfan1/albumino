23a444090419fb113ec72457f711024ce50b9dbe
bkfan1 <jpf8296@gmail.com>
2023-06-07 18:01:30 -0400

    Formatted code to improve readability

--- src/hooks/useIsMounted/index.js
+++ src/hooks/useIsMounted/index.js
@@ -1,11 +1,11 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 
-export const useIsMounted = ()=>{
-    const [isMounted, setIsMounted] = useState(false);
+export const useIsMounted = () => {
+  const [isMounted, setIsMounted] = useState(false);
 
-    useEffect(()=>{
-        setIsMounted(true);
-    }, [])
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
 
-    return {isMounted, setIsMounted}
-}
+  return { isMounted, setIsMounted };
+};
