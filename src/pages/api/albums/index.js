6f0aca777dddb5cbda74ec4a4a79e701a20bf1e9
bkfan1 <jpf8296@gmail.com>
2023-06-07 11:44:03 -0400

    Added comment related to api endpoint config object

--- src/pages/api/albums/index.js
+++ src/pages/api/albums/index.js
@@ -1,6 +1,9 @@
 import { getAccountAlbums } from "@/middlewares/account";
 import { createAlbum } from "@/middlewares/album";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../auth/[...nextauth]";
 
+// Disable bodyParser to allow Multer to handle files on incoming requests
 export const config = {
   api: {
     bodyParser: false,
@@ -10,7 +13,16 @@ export const config = {
 export default async function handler(req, res) {
   switch (req.method) {
     case "GET":
-      return await getAccountAlbums(req, res);
+      {
+        try {
+          const session = await getServerSession(req, res, authOptions);
+          const albums = await getAccountAlbums(session.user.accountId);
+          return res.status(200).json({ albums });
+        } catch (error) {
+          return res.status(500).json({});
+        }
+      }
+
       break;
 
     case "POST":
