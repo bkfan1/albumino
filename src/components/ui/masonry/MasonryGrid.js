59f646ac20c45ee79f8826ef65a32676d50916b2
bkfan1 <jpf8296@gmail.com>
2023-06-07 17:57:37 -0400

    Consumed PhotoVisor context inside component

--- src/components/ui/masonry/MasonryGrid.js
+++ src/components/ui/masonry/MasonryGrid.js
@@ -1,8 +1,11 @@
 import { Box, Grid } from "@chakra-ui/react";
 import MasonryGridItem from "./MasonryGridItem";
-import { PhotoVisorProvider } from "@/contexts/PhotoVisorContext";
+import { useContext } from "react";
+import { PhotoVisorContext } from "@/contexts/PhotoVisorContext";
 
-export default function MasonryGrid({ photos }) {
+export default function MasonryGrid({}) {
+  const { visorPhotos } = useContext(PhotoVisorContext);
+
   return (
     <>
       <Box width={"100%"}>
@@ -11,11 +14,9 @@ export default function MasonryGrid({ photos }) {
           gap={4}
           paddingBottom={4}
         >
-          <PhotoVisorProvider photos={photos}>
-            {photos.map((photo) => (
-              <MasonryGridItem key={photo.id} data={photo} />
-            ))}
-          </PhotoVisorProvider>
+          {visorPhotos.map((photo) => (
+            <MasonryGridItem key={photo.id} data={photo} />
+          ))}
         </Grid>
       </Box>
     </>
