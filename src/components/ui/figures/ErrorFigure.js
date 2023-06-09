7c1b6cb96380395c4929f0c9d3c6ef75499531dd
bkfan1 <jpf8296@gmail.com>
2023-06-09 17:46:46 -0400

    Created folder

---
+++ src/components/ui/figures/ErrorFigure.js
@@ -0,0 +1,21 @@
+import { Button, Heading, VStack, Text } from "@chakra-ui/react";
+import { MdWarning } from "react-icons/md";
+import { useRouter } from "next/router";
+
+export default function ErrorFigure({description}) {
+  const router = useRouter();
+  return (
+    <>
+      <VStack>
+        <Heading color={"yellow.500"} size={"2xl"}>
+          <MdWarning></MdWarning>
+        </Heading>
+        <Heading>Error</Heading>
+        <Text maxWidth={"2xl"} textAlign={"center"} >{description}</Text>
+        <Button onClick={() => router.push("/")} colorScheme="blue">
+          Back to app
+        </Button>
+      </VStack>
+    </>
+  );
+}
