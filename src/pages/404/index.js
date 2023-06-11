2bd09a6ba4367772aed61013617bef1d17346224
bkfan1 <jpf8296@gmail.com>
2023-06-10 22:19:06 -0400

    Removed unused imports

--- src/pages/404/index.js
+++ src/pages/404/index.js
@@ -1,13 +1,12 @@
 import ErrorFigure from "@/components/ui/figures/ErrorFigure";
-import { Button, Flex, Heading, Icon, VStack } from "@chakra-ui/react";
+import { Flex } from "@chakra-ui/react";
 
-
-export default function NotFoundErrorPage(){
-    return(
-        <>
-        <Flex minHeight={"100vh"} justifyContent={"center"} alignItems={"center"}>
-            <ErrorFigure description={"Resource not found"}/>
-        </Flex>
-        </>
-    )
+export default function NotFoundErrorPage() {
+  return (
+    <>
+      <Flex minHeight={"100vh"} justifyContent={"center"} alignItems={"center"}>
+        <ErrorFigure description={"Resource not found"} />
+      </Flex>
+    </>
+  );
 }
--- src/pages/500/index.js
+++ src/pages/500/index.js
@@ -1,13 +1,16 @@
 import ErrorFigure from "@/components/ui/figures/ErrorFigure";
-import { Button, Flex, Heading, Icon, VStack } from "@chakra-ui/react";
+import { Flex } from "@chakra-ui/react";
 
-
-export default function ServerErrorPage(){
-    return(
-        <>
-        <Flex minHeight={"100vh"} justifyContent={"center"} alignItems={"center"}>
-            <ErrorFigure description={"A server internal error has occurred. We apologize for the inconvenience. Our technical team has been notified about this issue and is working to resolve it as soon as possible. Please try again later."}/>
-        </Flex>
-        </>
-    )
+export default function ServerErrorPage() {
+  return (
+    <>
+      <Flex minHeight={"100vh"} justifyContent={"center"} alignItems={"center"}>
+        <ErrorFigure
+          description={
+            "A server internal error has occurred. We apologize for the inconvenience. Our technical team has been notified about this issue and is working to resolve it as soon as possible. Please try again later."
+          }
+        />
+      </Flex>
+    </>
+  );
 }
