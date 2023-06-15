b2633edeab7e078330bcb4c16c98f497294e2eac
bkfan1 <jpf8296@gmail.com>
2023-06-15 19:01:53 -0400

    Add conditional rendering inside Icon boxSize prop

--- src/components/ui/navigation/NavbarBrand.js
+++ src/components/ui/navigation/NavbarBrand.js
@@ -1,19 +1,25 @@
 import { useIsMounted } from "@/hooks/useIsMounted";
-import { Button, HStack, Skeleton, Text } from "@chakra-ui/react";
+import { HStack, Icon, Skeleton, Text } from "@chakra-ui/react";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import { SiGooglephotos } from "react-icons/si";
 
 export default function NavbarBrand() {
-  const {isMounted} = useIsMounted();
+  const { isMounted } = useIsMounted();
+  const router = useRouter();
+  const { pathname } = router;
+
+  const inSignPage = pathname === "/signin" || pathname === "/signup";
+
   return (
     <>
       <Link href="/">
         <Skeleton isLoaded={isMounted} paddingY={2} rounded={"md"}>
-        <HStack fontWeight={"bold"}>
-          <SiGooglephotos />
+          <HStack fontWeight={"bold"}>
+            <Icon as={SiGooglephotos} boxSize={6}></Icon>
 
-          <Text>Albumino</Text>
-        </HStack>
+            <Text fontSize={inSignPage ? "2xl" : ""}>Albumino</Text>
+          </HStack>
         </Skeleton>
       </Link>
     </>
