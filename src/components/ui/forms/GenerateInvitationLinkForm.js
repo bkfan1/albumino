bb708a185cf5e28b428fe9008995293246776948
bkfan1 <jpf8296@gmail.com>
2023-06-16 20:15:47 -0400

    Add onSubmit function to form

--- src/components/ui/forms/GenerateInvitationLinkForm.js
+++ src/components/ui/forms/GenerateInvitationLinkForm.js
@@ -10,32 +10,73 @@ import {
   Text,
   VStack,
   useClipboard,
+  useToast,
 } from "@chakra-ui/react";
+import axios from "axios";
+import { useRouter } from "next/router";
+import { useForm } from "react-hook-form";
 
 export default function GenerateInvitationLinkForm() {
-  const placeholder = "Invitation link goes here";
+  const { handleSubmit } = useForm();
   const { onCopy, value, setValue, hasCopied } = useClipboard("");
+  const router = useRouter();
+  const toast = useToast();
+  const { query } = router;
+  const { albumId } = query;
+
+  const onSubmit = async () => {
+    try {
+      const data = { sendEmail: false };
+      const resPromise = axios.post(`/api/album/${albumId}/invitation`, data);
+
+      toast.promise(resPromise, {
+        loading: { title: "Generating invitation link..." },
+        success: { title: "Invitation link generated succesfully" },
+        error: {
+          title:
+            "An error occurred while attempting to generate invitation link",
+        },
+      });
+
+      const res = await resPromise;
+      setValue(res.data.invitationLink);
+      
+    } catch (error) {
+      toast({
+        status: "error",
+        title: "An error occurred while attempting to generate invitation link",
+      });
+    }
+  };
+
+  const placeholder = "Invitation link goes here";
+
   return (
     <>
-    <VStack gap={2}>
+      <VStack as={"form"} gap={2} onSubmit={handleSubmit(onSubmit)}>
         <VStack width={"100%"}>
-        <Heading size={"md"} width={"100%"}>Generate link</Heading>
-        <Text width={"100%"}>Click in button to generate an invitation link (one use only)</Text>
+          <Heading size={"md"} width={"100%"}>
+            Generate link
+          </Heading>
+          <Text width={"100%"}>
+            Click in button to generate an invitation link (one use only)
+          </Text>
         </VStack>
-    <Flex width={"100%"}>
-    <Input
-          placeholder={placeholder}
-          value={value}
-          onChange={(e) => {
-            setValue(e.target.value);
-          }}
-          mr={2}
-        />
-        <Button onClick={onCopy}>{hasCopied ? "Copied!" : "Copy"}</Button>
-    </Flex>
-    <Button width={"100%"} colorScheme="green">Generate</Button>
-    </VStack>
-
+        <Flex width={"100%"}>
+          <Input
+            placeholder={placeholder}
+            value={value}
+            onChange={(e) => {
+              setValue(e.target.value);
+            }}
+            mr={2}
+          />
+          <Button onClick={onCopy}>{hasCopied ? "Copied!" : "Copy"}</Button>
+        </Flex>
+        <Button type="submit" width={"100%"} colorScheme="green">
+          Generate
+        </Button>
+      </VStack>
     </>
   );
 }
