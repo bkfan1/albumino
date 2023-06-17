7105eb5cc5d90ff0716e5cbbccda6263f9af8ac3
bkfan1 <jpf8296@gmail.com>
2023-06-16 20:30:15 -0400

    Remove try-catch block inside GET and POST cases

--- src/pages/api/albums/index.js
+++ src/pages/api/albums/index.js
@@ -1,32 +1,15 @@
-import { getAccountAlbums } from "@/middlewares/account";
+import { sendAccountAlbums } from "@/middlewares/account";
 import { createAlbum } from "@/middlewares/album";
-import { getServerSession } from "next-auth";
-import { authOptions } from "../auth/[...nextauth]";
-
-// Disable bodyParser to allow Multer to handle files on incoming requests
-export const config = {
-  api: {
-    bodyParser: false,
-  },
-};
 
 export default async function handler(req, res) {
   switch (req.method) {
     case "GET":
-      {
-        try {
-          const session = await getServerSession(req, res, authOptions);
-          const albums = await getAccountAlbums(session.user.accountId);
-          return res.status(200).json({ albums });
-        } catch (error) {
-          return res.status(500).json({});
-        }
-      }
-
+      return sendAccountAlbums(req, res);
       break;
 
     case "POST":
       return await createAlbum(req, res);
+      break;
 
     default:
       return await res.status(405).json({});
