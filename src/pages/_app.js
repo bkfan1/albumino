1590d9ad7c9f8384faf5cc7204ddc2d3dff22967
bkfan1 <jpf8296@gmail.com>
2023-06-24 17:42:47 -0400

    Wrap Component inside of PhotoVisorProvider and AlbumPageProvider

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,20 +4,19 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { SessionProvider } from "next-auth/react";
 import "@/styles/globals.css";
 
-
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
 }) {
   return (
     <SessionProvider session={session}>
-      <AlbumPageProvider>
-        <PhotoVisorProvider>
-          <ChakraProvider>
-            <Component {...pageProps} />
-          </ChakraProvider>
-        </PhotoVisorProvider>
-      </AlbumPageProvider>
+        <AlbumPageProvider>
+          <PhotoVisorProvider>
+            <ChakraProvider>
+              <Component {...pageProps} />
+            </ChakraProvider>
+          </PhotoVisorProvider>
+        </AlbumPageProvider>
     </SessionProvider>
   );
 }
