9ebdd62de30f9adfb233bae028ae19ae85025c20
bkfan1 <jpf8296@gmail.com>
2023-06-24 17:52:13 -0400

    Rebuild form from scratch

--- src/components/ui/forms/CreateAlbumForm.js
+++ src/components/ui/forms/CreateAlbumForm.js
@@ -1,76 +1,154 @@
 import {
   Box,
+  Button,
+  ButtonGroup,
   Flex,
   FormControl,
-  Heading,
+  HStack,
+  IconButton,
+  Image,
   Input,
   Text,
   VStack,
   useToast,
 } from "@chakra-ui/react";
-import axios from "axios";
+import { createRef, useContext, useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
+import { BsCheckLg } from "react-icons/bs";
+import { MdOutlineAddPhotoAlternate } from "react-icons/md";
+import MasonryGrid from "../masonry/MasonryGrid";
+import { nanoid } from "nanoid";
+import { PhotoVisorContext } from "@/contexts/PhotoVisorContext";
+import axios from "axios";
 
 export default function CreateAlbumForm() {
+  const { setVisorPhotos } = useContext(PhotoVisorContext);
+  const inputFileRef = createRef();
   const { register, handleSubmit } = useForm();
+  const [imagesToUpload, setImagesToUpload] = useState([]);
+  // const [previewImages, setPreviewImages] = useState([]);
   const toast = useToast();
+
+  const handleInputFileOnClick = () => {
+    inputFileRef.current.click();
+  };
+
+  const handleInputFileOnChange = (e) => {
+    const files = e.target.files;
+    const updatedImagesToUpload = [...imagesToUpload];
+    // const updatedPreviewImages = [...previewImages];
+
+    for (const file of files) {
+      updatedImagesToUpload.push(file);
+
+      const url = URL.createObjectURL(file);
+
+      // updatedPreviewImages.push({
+      //   id: nanoid(),
+      //   url,
+      // });
+    }
+
+    setImagesToUpload(updatedImagesToUpload);
+    // setPreviewImages(updatedPreviewImages);
+  };
 
   const onSubmit = async (data) => {
     try {
-      const formData = new FormData();
-
-      for (const file of data.files) {
-        formData.append("files", file);
-      }
-
-      const res = await axios.post("/api/albums", { name: data.name });
+      const res = await axios.post(`/api/albums`, { name: data.name });
       const albumId = res.data.albumId;
 
-      formData.append("albumId", albumId);
+      if (imagesToUpload.length > 0) {
+        const formData = new FormData();
 
-      if (data.files.length > 0) {
-        const config = {
-          headers: { "Content-Type": "multipart/form-data" },
-        };
+        for (const image of imagesToUpload) {
+          formData.append("files", image);
+        }
+
+        formData.append("albumId", albumId);
+
+        const config = { headers: { "Content-Type": "multipart/form-data" } };
 
         const res2 = await axios.post(`/api/photos`, formData, config);
       }
 
       toast({
         status: "success",
-        title: "Album created successfully",
+        title: "Album created succesfully",
       });
     } catch (error) {
       toast({
         status: "error",
-        title: "Error",
-        description: "Something went wrong while attempting to create album",
+        title: "An error occurred while trying to create album",
       });
     }
   };
+
+  // useEffect(() => {
+  //   setVisorPhotos(previewImages);
+  // }, [previewImages, setVisorPhotos]);
+
   return (
     <>
-      <Box width={"100%"} as="form" onSubmit={handleSubmit(onSubmit)}>
-        <FormControl width={"100%"}>
-          <Input
-            type="text"
-            placeholder="Add a title"
-            size={"lg"}
-            variant={"flushed"}
-            {...register("name")}
-          />
-        </FormControl>
+      <Box
+        as="form"
+        width={"100%"}
+        height={"100%"}
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <HStack>
+          <FormControl>
+            <Input
+              type="text"
+              placeholder="Album name"
+              variant={"flushed"}
+              fontSize={"2xl"}
+              {...register("name")}
+            />
+          </FormControl>
+          <ButtonGroup>
+            {imagesToUpload.length > 0 ? (
+              <IconButton
+                onClick={handleInputFileOnClick}
+                icon={<MdOutlineAddPhotoAlternate />}
+                rounded={"full"}
+              ></IconButton>
+            ) : (
+              ""
+            )}
+            <IconButton icon={<BsCheckLg />} rounded={"full"} />
+          </ButtonGroup>
+        </HStack>
 
-        <VStack paddingTop={4}>
-          <Heading size={"sm"}>Album is empty</Heading>
-          <input
-            type="file"
-            multiple
-            {...register("files")}
-            accept=".jpg, .jpeg, .png"
-          />
-        </VStack>
-        <Input type="submit" hidden />
+        <Flex
+          justifyContent={"center"}
+          alignItems={"center"}
+          width={"100%"}
+          height={"100%"}
+          py={6}
+        >
+          {/* <MasonryGrid /> */}
+          {imagesToUpload.length === 0 ? (
+            <VStack>
+              <Text fontWeight={"bold"}>Album is empty</Text>
+              <Button onClick={handleInputFileOnClick} colorScheme="blue">
+                Add photos
+              </Button>
+              <Image src="/empty_state_album.svg" alt="" />
+            </VStack>
+          ) : (
+            ""
+          )}
+        </Flex>
+
+        <Input
+          ref={inputFileRef}
+          onChange={(e) => handleInputFileOnChange(e)}
+          type="file"
+          accept=".jpg,.jpeg, .png"
+          multiple
+          display={"none"}
+        />
       </Box>
     </>
   );
