dc91c40375ea14e25aa0d270af136fd1f35a4db2
bkfan1 <jpf8296@gmail.com>
2023-06-24 17:39:16 -0400

    Add getAccountStorageData function

--- src/middlewares/account/index.js
+++ src/middlewares/account/index.js
@@ -3,10 +3,11 @@ import Account from "@/database/models/Account";
 import Album from "@/database/models/Album";
 import Photo from "@/database/models/Photo";
 import accountSchema from "@/utils/joi/schemas/account";
-import { hash } from "bcrypt";
+import { compare, hash } from "bcrypt";
 import { getFirstPhotoInAlbum } from "../photo";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/pages/api/auth/[...nextauth]";
+import { calculateFolderSize } from "@/utils/firebase-app";
 
 export const accountExists = async (accountId) => {
   try {
@@ -78,7 +79,7 @@ export const getAccountPhotos = async (accountId) => {
   }
 };
 
-export const getAccountAlbums = async (accountId) => {
+export const getAccountAlbums = async (accountId, albumType = "own") => {
   try {
     const exists = await accountExists(accountId);
 
@@ -88,13 +89,19 @@ export const getAccountAlbums = async (accountId) => {
 
     const db = await connection();
 
-    const accountAlbums = await Album.find({
-      author_account_id: accountId,
-    });
+    let foundAlbums;
+
+    if (albumType === "own") {
+      foundAlbums = await Album.find({ author_account_id: accountId });
+    }
+
+    if (albumType === "shared") {
+      foundAlbums = await Album.find({ contributors: accountId });
+    }
 
     const albums = [];
 
-    for (const album of accountAlbums) {
+    for (const album of foundAlbums) {
       const albumPhotos = await Photo.find({ albums: album._id });
       const cover = await getFirstPhotoInAlbum(album._id.toString());
 
@@ -127,17 +134,56 @@ export const sendAccountAlbums = async (req, res) => {
     const existsAccount = await accountExists(session.user.accountId);
 
     if (!existsAccount) {
-      return res.status(400).json({ message: "Account not found" });
+      return res.status(404).json({ message: "Account not found" });
     }
 
-    const albums = await getAccountAlbums(session.user.accountId);
+    const albums = await getAccountAlbums(session.user.accountId, "own");
 
     return res.status(200).json({ albums });
   } catch (error) {
+    console.log(error)
+    return res.status(500).json({
+      message: "An error occurred while attempting to fetch account albums",
+    });
+  }
+};
+
+export const getAccountStorageData = async (req, res) => {
+  try {
+    const session = await getServerSession(req, res, authOptions);
+
+    const existsAccount = await accountExists(session.user.accountId);
+
+    if (!existsAccount) {
+      return res.status(404).json({ message: "Account not found" });
+    }
+
+    const folderPath = `/users/${session.user.accountId}`;
+
+    // Getting used storage value in bytes
+    const usedStorageInBytes = await calculateFolderSize(folderPath);
+
+    // Converting used storage value to MB
+    const mb = usedStorageInBytes / 1e6;
+
+    // Converting used storage value to percentage
+    const percent = (mb * 100) / 100;
+
+    const storage = {
+      used: {
+        bytes: usedStorageInBytes,
+        mb,
+        percent,
+      },
+    };
+
+    return res.status(200).json({ storage });
+  } catch (error) {
+    console.log(error);
     return res
       .status(500)
       .json({
-        message: "An error occurred while attempting to fetch account albums",
+        message: "An error occurred while obtaining account storage data",
       });
   }
 };
