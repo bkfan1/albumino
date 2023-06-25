d0aa39fab50730059bb45082f71964de6f595723
bkfan1 <jpf8296@gmail.com>
2023-06-24 22:17:45 -0400

    Fix logic inside handleInputFileOnChange and onSubmit functions

--- src/components/ui/forms/CreateAlbumForm.js
+++ src/components/ui/forms/CreateAlbumForm.js
@@ -5,10 +5,12 @@ import {
   Flex,
   FormControl,
   HStack,
+  Heading,
   IconButton,
   Image,
   Input,
   Text,
+  Tooltip,
   VStack,
   useToast,
 } from "@chakra-ui/react";
@@ -20,13 +22,16 @@ import MasonryGrid from "../masonry/MasonryGrid";
 import { nanoid } from "nanoid";
 import { PhotoVisorContext } from "@/contexts/PhotoVisorContext";
 import axios from "axios";
+import { hasInvalidFileType } from "@/utils/validation";
+import { allowedPhotosFileTypes } from "@/utils/validation";
 
 export default function CreateAlbumForm() {
-  const { setVisorPhotos } = useContext(PhotoVisorContext);
+  const { visorPhotos, setVisorPhotos, setCurrentPhoto } =
+    useContext(PhotoVisorContext);
   const inputFileRef = createRef();
+
   const { register, handleSubmit } = useForm();
-  const [imagesToUpload, setImagesToUpload] = useState([]);
-  // const [previewImages, setPreviewImages] = useState([]);
+
   const toast = useToast();
 
   const handleInputFileOnClick = () => {
@@ -34,23 +39,31 @@ export default function CreateAlbumForm() {
   };
 
   const handleInputFileOnChange = (e) => {
-    const files = e.target.files;
-    const updatedImagesToUpload = [...imagesToUpload];
-    // const updatedPreviewImages = [...previewImages];
+    let files = [...e.target.files];
+    const updatedVisorPhotos = [...visorPhotos];
 
-    for (const file of files) {
-      updatedImagesToUpload.push(file);
+    const hasInvalidFiles = hasInvalidFileType(files, allowedPhotosFileTypes);
 
-      const url = URL.createObjectURL(file);
-
-      // updatedPreviewImages.push({
-      //   id: nanoid(),
-      //   url,
-      // });
+    if (hasInvalidFiles) {
+      files = files.filter((file) =>
+        allowedPhotosFileTypes.includes(file.type)
+      );
     }
 
-    setImagesToUpload(updatedImagesToUpload);
-    // setPreviewImages(updatedPreviewImages);
+    for (const file of files) {
+      const url = URL.createObjectURL(file);
+      const id = nanoid();
+
+      const photoToUpload = {
+        id,
+        file,
+        url,
+      };
+
+      updatedVisorPhotos.push(photoToUpload);
+    }
+
+    setVisorPhotos(updatedVisorPhotos);
   };
 
   const onSubmit = async (data) => {
@@ -58,11 +71,22 @@ export default function CreateAlbumForm() {
       const res = await axios.post(`/api/albums`, { name: data.name });
       const albumId = res.data.albumId;
 
-      if (imagesToUpload.length > 0) {
+      // We are going to use the photos from visorPhotos state variable
+      // Since we can take advantage of MasonryGrid to display the uploading photos
+      // for previewing them in this form (and saving us the need of create another component for this purpose)
+
+      // NOTE: I dont think that the described approach above is good to handle this form, but
+      // I didn't came with a best solution, so... feel free to open an issue in the app's repository
+      // about how to change this approach to a better one
+
+      if (visorPhotos.length > 0) {
         const formData = new FormData();
 
-        for (const image of imagesToUpload) {
-          formData.append("files", image);
+        for (const photo of visorPhotos) {
+          const file = photo.file;
+          console.log(file);
+
+          formData.append("files", file);
         }
 
         formData.append("albumId", albumId);
@@ -76,6 +100,9 @@ export default function CreateAlbumForm() {
         status: "success",
         title: "Album created succesfully",
       });
+
+      setVisorPhotos([]);
+      setCurrentPhoto({});
     } catch (error) {
       toast({
         status: "error",
@@ -83,10 +110,6 @@ export default function CreateAlbumForm() {
       });
     }
   };
-
-  // useEffect(() => {
-  //   setVisorPhotos(previewImages);
-  // }, [previewImages, setVisorPhotos]);
 
   return (
     <>
@@ -103,41 +126,43 @@ export default function CreateAlbumForm() {
               placeholder="Album name"
               variant={"flushed"}
               fontSize={"2xl"}
-              {...register("name")}
+              {...register("name", {
+                required: { value: true, message: "This field is required" },
+              })}
             />
           </FormControl>
           <ButtonGroup>
-            {imagesToUpload.length > 0 ? (
+            <Tooltip label="Add photos">
               <IconButton
                 onClick={handleInputFileOnClick}
                 icon={<MdOutlineAddPhotoAlternate />}
                 rounded={"full"}
-              ></IconButton>
-            ) : (
-              ""
-            )}
-            <IconButton icon={<BsCheckLg />} rounded={"full"} />
+                display={visorPhotos.length > 0 ? "flex" : "none"}
+              />
+            </Tooltip>
+            <Tooltip label="Create album">
+              <IconButton type="submit" icon={<BsCheckLg />} rounded={"full"} />
+            </Tooltip>
           </ButtonGroup>
         </HStack>
 
         <Flex
-          justifyContent={"center"}
-          alignItems={"center"}
           width={"100%"}
           height={"100%"}
+          alignItems={visorPhotos.length === 0 && "center"}
+          justifyContent={visorPhotos.length === 0 && "center"}
           py={6}
         >
-          {/* <MasonryGrid /> */}
-          {imagesToUpload.length === 0 ? (
+          {visorPhotos.length > 0 ? (
+            <MasonryGrid />
+          ) : (
             <VStack>
-              <Text fontWeight={"bold"}>Album is empty</Text>
+              <Image src={"/empty_state_album.svg"} alt="" />
+              <Heading size={"md"}>Album is empty</Heading>
               <Button onClick={handleInputFileOnClick} colorScheme="blue">
                 Add photos
               </Button>
-              <Image src="/empty_state_album.svg" alt="" />
             </VStack>
-          ) : (
-            ""
           )}
         </Flex>
 
