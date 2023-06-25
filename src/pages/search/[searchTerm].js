4756e3c288260180cf69d7e46339cd41ee374aea
bkfan1 <jpf8296@gmail.com>
2023-06-24 22:20:22 -0400

    Update imports

--- src/pages/search/[searchTerm].js
+++ src/pages/search/[searchTerm].js
@@ -1,15 +1,12 @@
-import Layout from "@/components/Layout";
-import { Flex, Heading } from "@chakra-ui/react";
+import Layout from "@/components/ui/Layout";
+import { Flex } from "@chakra-ui/react";
 
-
-export default function SearchResults(){
-    return(
-        <>
-        <Layout>
-            <Flex as="main" flex={6}>
-
-            </Flex>
-        </Layout>
-        </>
-    )
+export default function SearchResults() {
+  return (
+    <>
+      <Layout>
+        <Flex as="main" flex={6}></Flex>
+      </Layout>
+    </>
+  );
 }
