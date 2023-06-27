a655ff9f89d1ee238402be9edddecdec51e62d0f
bkfan1 <jpf8296@gmail.com>
2023-06-26 20:09:26 -0400

    Use UseEffect to handle app route changes

--- src/contexts/AlbumPageContext.js
+++ src/contexts/AlbumPageContext.js
@@ -4,9 +4,28 @@ import { createContext, useEffect, useState } from "react";
 export const AlbumPageContext = createContext();
 
 export const AlbumPageProvider = ({ children }) => {
+  const router = useRouter();
+  const {pathname} = router;
+
   const [inAlbumPage, setInAlbumPage] = useState(false);
   const [isAlbumOwner, setIsAlbumOwner] = useState(false);
   const [showAlbumSettings, setShowAlbumSettings] = useState(false);
+
+  useEffect(()=>{
+
+    const updateInAlbumPage = ()=>{
+      if(pathname === "/album/[albumId]"){
+        setInAlbumPage(true);
+      }
+    }
+
+    updateInAlbumPage();
+
+    return ()=>{
+      setInAlbumPage(false);
+    }
+
+  }, [pathname])
 
   return (
     <>
