438ecf1151fc64d4a5e6a77c09e7d1da3dbff01b
bkfan1 <jpf8296@gmail.com>
2023-06-29 19:30:09 -0400

    Comment handler functions and context consumption

--- src/components/ui/cards/AvailableAlbumCard.js
+++ src/components/ui/cards/AvailableAlbumCard.js
@@ -1,57 +1,60 @@
-import { PhotoVisorContext } from "@/contexts/PhotoVisorContext";
+// import { PhotoVisorContext } from "@/contexts/PhotoVisorContext";
 import { Box, HStack, Icon, Image, Text, VStack } from "@chakra-ui/react";
-import { useContext } from "react";
+// import { useContext } from "react";
 import { MdOutlinePhotoAlbum } from "react-icons/md";
 import { useToast } from "@chakra-ui/react";
 import axios from "axios";
 
 export default function AvailableAlbumCard({ album }) {
-  const {
-    currentPhoto,
-    setCurrentPhoto,
-    setShowAvailableAlbums,
-    setAvailableAlbums,
-  } = useContext(PhotoVisorContext);
+  // const {
+  //   currentPhoto,
+  //   updateCurrentPhoto,
+  //   setShowAvailableAlbums,
+  //   setAvailableAlbums,
+  // } = useContext(PhotoVisorContext);
 
-  const isPhotoAlreadyOnAlbum =
-    currentPhoto.albums.includes(album.id) ||
-    album.photos.find((albumPhoto) => albumPhoto.id === currentPhoto.id)
-      ? true
-      : false;
+  // const isPhotoAlreadyOnAlbum =
+  //   currentPhoto.albums.includes(album.id) ||
+  //   album.photos.find((albumPhoto) => albumPhoto.id === currentPhoto.id)
+  //     ? true
+  //     : false;
 
-  const toast = useToast();
+  // const toast = useToast();
 
-  const handleAddToAlbum = async (albumId) => {
-    try {
-      const addPromise = axios.put(
-        `/api/album/${albumId}/photos/${currentPhoto.id}`
-      );
+  // const handleAddToAlbum = async (albumId) => {
+  //   try {
+  //     const addPromise = axios.put(
+  //       `/api/album/${albumId}/photos/${currentPhoto.id}`
+  //     );
 
-      toast.promise(addPromise, {
-        loading: { title: "Adding photo to album..." },
-        success: { title: "Photo added to album succesfully" },
-        error: { title: "Error while trying to add photo to album" },
-      });
+  //     toast.promise(addPromise, {
+  //       loading: { title: "Adding photo to album..." },
+  //       success: { title: "Photo added to album succesfully" },
+  //       error: { title: "Error while trying to add photo to album" },
+  //     });
 
-      await addPromise;
+  //     await addPromise;
 
-      setCurrentPhoto({
-        ...currentPhoto,
-        albums: [...currentPhoto.albums, { id: albumId }],
-      });
-      setShowAvailableAlbums(false);
-      setAvailableAlbums([]);
-    } catch (error) {
-      console.log(error);
-      toast({
-        status: "error",
-        title: "Error",
-        description: "An error occurred while trying to add photo to album.",
-        duration: 5000,
-        isClosable: false,
-      });
-    }
-  };
+  //     const updatedCurrentPhoto = {
+  //       ...currentPhoto,
+  //       albums: [...currentPhoto.albums, { id: albumId }],
+  //     }
+
+  //     updateCurrentPhoto(updatedCurrentPhoto);
+
+  //     setShowAvailableAlbums(false);
+  //     setAvailableAlbums([]);
+  //   } catch (error) {
+  //     console.log(error);
+  //     toast({
+  //       status: "error",
+  //       title: "Error",
+  //       description: "An error occurred while trying to add photo to album.",
+  //       duration: 5000,
+  //       isClosable: false,
+  //     });
+  //   }
+  // };
 
   return (
     <>
