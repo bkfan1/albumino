caa866899ab9144d583f18c605262c1e81872bc5
bkfan1 <jpf8296@gmail.com>
2023-06-29 19:38:42 -0400

    Remove isOpen and onClose props from Modal component

--- src/components/ui/visor/PhotoVisor.js
+++ src/components/ui/visor/PhotoVisor.js
@@ -2,14 +2,15 @@ import { useContext } from "react";
 import { Modal, ModalOverlay } from "@chakra-ui/react";
 import PhotoVisorHeader from "./PhotoVisorHeader";
 import PhotoVisorBody from "./PhotoVisorBody";
-import { PhotoVisorContext } from "@/contexts/PhotoVisorContext";
 
 export default function PhotoVisor({}) {
-  const { isOpen, onClose } = useContext(PhotoVisorContext);
 
   return (
     <>
-      <Modal isOpen={isOpen} onClose={onClose} blockScrollOnMount={true} isCentered>
+      <Modal
+        blockScrollOnMount={true}
+        isCentered
+      >
         <ModalOverlay>
           <PhotoVisorHeader />
           <PhotoVisorBody />
