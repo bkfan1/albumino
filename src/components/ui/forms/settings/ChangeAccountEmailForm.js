1a178e9d7518fc131e6ec6b84535c199f13ef520
bkfan1 <jpf8296@gmail.com>
2023-07-20 18:33:54 -0400

    Move settings related components inside folder

---
+++ src/components/ui/forms/settings/ChangeAccountEmailForm.js
@@ -0,0 +1,88 @@
+import { regex } from "@/utils/regex";
+import {
+  Button,
+  FormControl,
+  Heading,
+  Input,
+  VStack,
+  useToast,
+} from "@chakra-ui/react";
+import axios from "axios";
+import { useForm } from "react-hook-form";
+
+export default function ChangeAccountEmailForm() {
+  const { register, handleSubmit } = useForm();
+
+  const toast = useToast();
+
+  const onSubmit = async (data) => {
+    const res = axios.put("/api/account/email", data);
+    toast.promise(res, {
+      loading: { title: "Changing email..." },
+      success: { title: "Email updated successfully" },
+      error: { title: "An error occurred while trying to update email" },
+    });
+  };
+  return (
+    <>
+      <VStack
+        as="form"
+        maxWidth={"360px"}
+        spacing={2}
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <Heading size={"md"} width={"100%"}>
+          Change account email
+        </Heading>
+        <FormControl>
+          <Input
+            type="email"
+            isRequired
+            placeholder="Old email"
+            {...register("oldEmail", {
+              required: { value: true, message: "This field is required" },
+              pattern: {
+                value: regex.email,
+                message: "Type a valid email address",
+              },
+            })}
+          />
+        </FormControl>
+
+        <FormControl>
+          <Input
+            type="email"
+            isRequired
+            placeholder="New email"
+            {...register("newEmail", {
+              required: { value: true, message: "This field is required" },
+              pattern: {
+                value: regex.email,
+                message: "Type a valid email address",
+              },
+            })}
+          />
+        </FormControl>
+
+        <FormControl>
+          <Input
+            type="email"
+            isRequired
+            placeholder="Type new email again"
+            {...register("confirmNewEmail", {
+              required: { value: true, message: "This field is required" },
+              pattern: {
+                value: regex.email,
+                message: "Type a valid email address",
+              },
+            })}
+          />
+        </FormControl>
+
+        <Button type="submit" width={"100%"} colorScheme={"blue"}>
+          Change
+        </Button>
+      </VStack>
+    </>
+  );
+}
---
+++ src/components/ui/forms/settings/ChangeAccountPasswordForm.js
@@ -0,0 +1,80 @@
+import {
+  Button,
+  FormControl,
+  Heading,
+  Input,
+  VStack,
+  useToast,
+} from "@chakra-ui/react";
+import axios from "axios";
+import { useForm } from "react-hook-form";
+
+export default function ChangeAccountPasswordForm() {
+  const { register, handleSubmit } = useForm();
+
+  const toast = useToast();
+
+  const onSubmit = async (data) => {
+    const res = axios.put("/api/account/password", data);
+    toast.promise(res, {
+      loading: { title: "Changing password..." },
+      success: { title: "Password updated successfully" },
+      error: { title: "An error occurred while trying to update password" },
+    });
+  };
+  return (
+    <>
+      <VStack
+        as="form"
+        maxWidth={"360px"}
+        spacing={2}
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <Heading size={"md"} width={"100%"}>
+          Change account password
+        </Heading>
+        <FormControl>
+          <Input
+            type="password"
+            {...register("oldPassword", {
+              required: { value: true, message: "This field is required" },
+            })}
+            placeholder="Old password"
+          />
+        </FormControl>
+
+        <FormControl>
+          <Input
+            type="password"
+            {...register("newPassword", {
+              required: { value: true, message: "This field is required" },
+              minLength: {
+                value: 8,
+                message: "Password has to be at least 8 characters long",
+              },
+            })}
+            placeholder="New password"
+          />
+        </FormControl>
+
+        <FormControl>
+          <Input
+            type="password"
+            {...register("confirmNewPassword", {
+              required: { value: true, message: "This field is required" },
+              minLength: {
+                value: 8,
+                message: "Password has to be at least 8 characters long",
+              },
+            })}
+            placeholder="Type new password again"
+          />
+        </FormControl>
+
+        <Button type="submit" width={"100%"} colorScheme={"blue"}>
+          Change
+        </Button>
+      </VStack>
+    </>
+  );
+}
