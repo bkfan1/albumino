c78f02c405878f8c5afff6455bdfdadda1624cd6
bkfan1 <jpf8296@gmail.com>
2023-07-20 18:29:53 -0400

    Create component to handle undo selections from masonry grids

---
+++ src/components/ui/UndoSelectionButton.js
@@ -0,0 +1,25 @@
+import { MasonryGridContext } from "@/contexts/MasonryGridContext";
+import { HStack, IconButton, Text, Tooltip } from "@chakra-ui/react";
+import { useContext } from "react";
+import { BsXLg } from "react-icons/bs";
+
+export default function UndoSelectionButton() {
+  const { selectedPhotos, handleUndoSelection } =
+    useContext(MasonryGridContext);
+
+  return (
+    <>
+      <HStack>
+        <Tooltip label="Undo selection">
+          <IconButton
+            onClick={handleUndoSelection}
+            icon={<BsXLg />}
+            variant={"ghost"}
+            rounded={"full"}
+          />
+        </Tooltip>
+        <Text>{selectedPhotos.length} selected</Text>
+      </HStack>
+    </>
+  );
+}
