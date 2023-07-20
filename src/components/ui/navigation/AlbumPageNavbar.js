c746fbb54ec05d8b4e841206d058a0a776d62511
bkfan1 <jpf8296@gmail.com>
2023-07-20 18:35:35 -0400

    Create component for usage in AlbumPageLayout

---
+++ src/components/ui/navigation/AlbumPageNavbar.js
@@ -0,0 +1,78 @@
+import {
+  Button,
+  Flex,
+  HStack,
+  IconButton,
+  Skeleton,
+  SkeletonCircle,
+  Text,
+  Tooltip,  
+} from "@chakra-ui/react";
+import { BsArrowLeft } from "react-icons/bs";
+
+import { MasonryGridContext } from "@/contexts/MasonryGridContext";
+import { useContext, useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import SelectedPhotosActionsMenu from "./menus/SelectedPhotosActionsMenu";
+import AlbumUploadPhotosMenu from "./menus/AlbumUploadPhotosMenu";
+import AlbumOptionsMenu from "./menus/AlbumOptionsMenu";
+import { useIsMounted } from "@/hooks/useIsMounted";
+import UndoSelectionButton from "../UndoSelectionButton";
+import { useIsNavbarFixed } from "@/hooks/useIsNavbarFixed";
+import { AlbumPageContext } from "@/contexts/AlbumPageContext";
+
+export default function AlbumPageNavbar() {
+  const {isAlbumOwner} = useContext(AlbumPageContext);
+  const { selectedPhotos } = useContext(MasonryGridContext);
+  const { isMounted } = useIsMounted();
+  const router = useRouter();
+
+  const {isNavbarFixed} = useIsNavbarFixed();
+
+  return (
+    <>
+      <Flex
+        width={"100%"}
+        alignItems={"center"}
+        justifyContent={"space-between"}
+        paddingX={4}
+        paddingY={4}
+
+        position={isNavbarFixed ? "fixed" : "static"}
+      top={0}
+      left={0}
+      right={0}
+      zIndex={10}
+      backgroundColor={"white"}
+      boxShadow={isNavbarFixed ? "md" : "none"}
+      >
+        {selectedPhotos.length > 0 ? (
+          <UndoSelectionButton/>
+          
+
+        ) : <SkeletonCircle isLoaded={isMounted}>
+        <IconButton
+          icon={<BsArrowLeft />}
+          size={"md"}
+          rounded={"full"}
+          variant={"ghost"}
+          padding={1}
+          onClick={() => router.back()}
+        />
+      </SkeletonCircle>}
+
+        <Skeleton isLoaded={isMounted} rounded={"md"}>
+          <HStack gap={4}>
+            {selectedPhotos.length > 0 ? (
+              <SelectedPhotosActionsMenu />
+            ) : (
+              <AlbumUploadPhotosMenu />
+            )}
+
+            <AlbumOptionsMenu />
+          </HStack>
+        </Skeleton>
+      </Flex>
+    </>
+  );
+}
