ec4cf48d3c514b3af63cf4f2dfb722ddf41c6424
bkfan1 <jpf8296@gmail.com>
2023-07-20 18:55:05 -0400

    Add message property inside json response

--- src/pages/api/photos/index.js
+++ src/pages/api/photos/index.js
@@ -12,14 +12,14 @@ export default async function handler(req, res) {
   switch (req.method) {
     case "GET":
       return await sendAccountPhotos(req, res);
-    
+
     case "POST":
       return await uploadPhotos(req, res);
 
       break;
 
     default:
-      return res.status(405).json({});
+      return res.status(405).json({message: ""});
       break;
   }
 }
