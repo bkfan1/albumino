374cf2d58524d2666132973c703ac4c8670aabb3
bkfan1 <jpf8296@gmail.com>
2023-07-20 19:02:33 -0400

    Improve logic inside updateAccountPassword function

--- src/middlewares/account/password/index.js
+++ src/middlewares/account/password/index.js
@@ -3,6 +3,8 @@ import { accountExists } from "..";
 import Account from "@/database/models/Account";
 import { compare, hash } from "bcrypt";
 import { updatePasswordSchema } from "@/utils/joi/schemas/account/password";
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
+import { passwordSalt } from "@/utils/constants";
 
 export const updateAccountPassword = async (req, res) => {
   try {
@@ -13,29 +15,42 @@ export const updateAccountPassword = async (req, res) => {
       return res.status(404).json({ message: "Account not found" });
     }
 
+    // This only validates if req.body has oldPassword, newPassword and confirmNewPassword,
+    // and if newPassword and confirmNewPassword are the same
+    // The validation for checking if oldPassword its equal to account current password
+    // is in a few lines bellow
     const validationResult = await updatePasswordSchema.validateAsync(req.body);
 
-    if(validationResult.error){
+    if (validationResult.error) {
       return res.status(400).json({});
     }
 
     const account = await Account.findById(session.user.accountId);
 
-    const matchWithAccountPassword = await compare(req.body.oldPassword, account.password);
+    // Comparing oldPassword with the account current password
+    const matchWithAccountPassword = await compare(
+      req.body.oldPassword,
+      account.password
+    );
 
     // if the incoming value in req.body.oldPassword is not the same as the account current password
-    if (!(matchWithAccountPassword)) {
-      return res.status(400).json({ message: "Passwords dont match" });
+    if (!matchWithAccountPassword) {
+      return res.status(400).json({ message: "Old passwords don't match" });
     }
 
-    const isCurrentPasswordMatchingNewPassword = await compare(req.body.newPassword, account.password)
+    const isCurrentPasswordMatchingNewPassword = await compare(
+      req.body.newPassword,
+      account.password
+    );
 
     // If the new password it's the same as the account current password, then...
-    if(isCurrentPasswordMatchingNewPassword){
-      return res.status(400).json({message:""})
+    if (isCurrentPasswordMatchingNewPassword) {
+      return res.status(400).json({
+        message: "New password has to be different from your current password",
+      });
     }
 
-    const newPasswordHash = await hash(req.body.newPassword, 10);
+    const newPasswordHash = await hash(req.body.newPassword, passwordSalt);
 
     const updatedAccount = await Account.findByIdAndUpdate(
       session.user.accountId,
