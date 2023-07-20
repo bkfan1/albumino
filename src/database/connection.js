b052c01ed478248b6aa0c3f60a36ec263fdf89e4
bkfan1 <jpf8296@gmail.com>
2023-07-20 19:00:25 -0400

    Return connection inside db variable

--- src/database/connection.js
+++ src/database/connection.js
@@ -2,7 +2,8 @@ import { connect } from "mongoose";
 
 export default async function connection() {
   try {
-    return connect(process.env.MONGODB_URI);
+    const db = await connect(process.env.MONGODB_URI);
+    return db;
   } catch (error) {
     throw Error("Error while attempting to connect to database.");
   }
