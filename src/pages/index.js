803ddedc748755ff6309dfc8a4490c0ef7e41989
bkfan1 <jpf8296@gmail.com>
2023-07-20 18:53:50 -0400

    Add Flex component with content

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,27 @@
 import { getServerSession } from "next-auth";
 import { authOptions } from "./api/auth/[...nextauth]";
+import { Button, ButtonGroup, Flex, Heading } from "@chakra-ui/react";
+import NavbarBrand from "@/components/ui/navigation/NavbarBrand";
 
 export default function Home() {
-  return <></>;
+  return (
+    <>
+      <Flex
+        alignItems={"center"}
+        justifyContent={"space-between"}
+        width={"100%"}
+      >
+        <NavbarBrand />
+        <ButtonGroup>
+          <Button colorScheme="blue">Sign Up</Button>
+          <Button variant={"ghost"}> Log In</Button>
+        </ButtonGroup>
+      </Flex>
+      <Flex minHeight={"100vh"}>
+        <Heading>Upload photos and share albums</Heading>
+      </Flex>
+    </>
+  );
 }
 
 export async function getServerSideProps({ req, res }) {
