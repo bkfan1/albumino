70041c13e4f58c63188be51e80d7d30e1a081cb9
bkfan1 <jpf8296@gmail.com>
2023-07-24 20:51:12 -0400

    Wrap page with PhotoVisorProvider

--- src/pages/photos/index.js
+++ src/pages/photos/index.js
@@ -6,18 +6,21 @@ import { getAccountPhotos } from "@/middlewares/account";
 import { MasonryGridProvider } from "@/contexts/MasonryGridContext";
 import PhotoVisor from "@/components/ui/visor/PhotoVisor";
 import PhotosPageLayout from "@/components/ui/layouts/PhotosPageLayout";
+import { PhotoVisorProvider } from "@/contexts/PhotoVisorContext";
 
 export default function PhotosPage({ photos }) {
   return (
     <>
-      <MasonryGridProvider photos={photos}>
-        <PhotosPageLayout>
-          <Flex as={"main"} flex={6} paddingRight={4} paddingTop={2}>
-            <MasonryGrid />
-            <PhotoVisor />
-          </Flex>
-        </PhotosPageLayout>
-      </MasonryGridProvider>
+      <PhotoVisorProvider>
+        <MasonryGridProvider photos={photos}>
+          <PhotosPageLayout>
+            <Flex as={"main"} flex={6} paddingRight={4} paddingTop={2}>
+              <MasonryGrid />
+              <PhotoVisor />
+            </Flex>
+          </PhotosPageLayout>
+        </MasonryGridProvider>
+      </PhotoVisorProvider>
     </>
   );
 }
