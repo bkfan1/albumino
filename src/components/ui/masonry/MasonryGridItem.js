d9ef4eae754928e7d6cdef6045b585c757f6d249
bkfan1 <jpf8296@gmail.com>
2023-07-24 20:41:34 -0400

    Add conditional rendering inside return function

--- src/components/ui/masonry/MasonryGridItem.js
+++ src/components/ui/masonry/MasonryGridItem.js
@@ -10,30 +10,19 @@ import { useContext, useState } from "react";
 import { useIsMounted } from "@/hooks/useIsMounted";
 import { useRouter } from "next/router";
 import { MasonryGridContext } from "@/contexts/MasonryGridContext";
-
-const getRandomSkeletonSize = () => {
-  const skeletonSizes = ["xs", "sm", "md"];
-  const index = Math.floor(Math.random() * 2);
-
-  return skeletonSizes[index];
-};
+import { PhotoVisorContext } from "@/contexts/PhotoVisorContext";
 
 export default function MasonryGridItem({ data, masonryType }) {
-  // const {data: session, status} = useSession();
-  // const {inAlbumPage} = useContext(AlbumPageContext);
-
   const { isMounted } = useIsMounted();
   const { id, url, albums } = data;
+
   const router = useRouter();
   const { query } = router;
 
-  const {
-    onOpen,
-    masonryPhotos,
-    setCurrentVisorPhoto,
-    selectedPhotos,
-    setSelectedPhotos,
-  } = useContext(MasonryGridContext);
+  const { onOpen, setCurrentVisorPhoto } = useContext(PhotoVisorContext);
+
+  const { masonryPhotos, selectedPhotos, setSelectedPhotos } =
+    useContext(MasonryGridContext);
 
   const [showCheckbox, setShowCheckbox] = useState(false);
 
@@ -62,61 +51,58 @@ export default function MasonryGridItem({ data, masonryType }) {
   const alreadyInAlbum = albums.includes(query.albumId);
 
   const selected = selectedPhotos.some((photo) => photo.id === id);
-
-  if (!isMounted) {
-    return (
-      <>
-        <Skeleton height={getRandomSkeletonSize()}></Skeleton>
-      </>
-    );
-  }
-
   return (
     <>
-      <GridItem>
-        <Box
-          position="relative"
-          overflow={"hidden"}
-          onMouseEnter={() => setShowCheckbox(true)}
-          onMouseLeave={() => setShowCheckbox(false)}
-          _hover={
-            masonryType === "form" && alreadyInAlbum
-              ? {}
-              : {
-                  cursor: "pointer",
-                  opacity: "85%",
-                  transition: "180ms ease-in-out",
-                }
-          }
-          opacity={masonryType === "form" && alreadyInAlbum ? "70%" : ""}
-        >
-          <Checkbox
-            onChange={() => handleOnSelect(id)}
-            isChecked={selected}
-            position={"absolute"}
-            m={2}
-            isDisabled={masonryType === "form" && alreadyInAlbum}
-            display={showCheckbox || selected ? "flex" : "none"}
-          />
-
-          <Tooltip
-            label={
+      {isMounted ? (
+        <GridItem>
+          <Box
+            position="relative"
+            overflow={"hidden"}
+            onMouseEnter={() => setShowCheckbox(true)}
+            onMouseLeave={() => setShowCheckbox(false)}
+            _hover={
               masonryType === "form" && alreadyInAlbum
-                ? "Photo is already on album"
-                : ""
+                ? {}
+                : {
+                    cursor: "pointer",
+                    opacity: "85%",
+                    transition: "180ms ease-in-out",
+                  }
             }
+            opacity={masonryType === "form" && alreadyInAlbum ? "70%" : ""}
           >
-            <Image
-              src={url}
-              alt={"Photo"}
-              w="100%"
-              h="auto"
-              rounded="md"
-              onClick={() => handleToggleVisor(data)}
+            <Checkbox
+              onChange={() => handleOnSelect(id)}
+              isChecked={selected}
+              position={"absolute"}
+              m={2}
+              isDisabled={masonryType === "form" && alreadyInAlbum}
+              display={showCheckbox || selected ? "flex" : "none"}
             />
-          </Tooltip>
-        </Box>
-      </GridItem>
+
+            <Tooltip
+              label={
+                masonryType === "form" && alreadyInAlbum
+                  ? "Photo is already on album"
+                  : ""
+              }
+            >
+              <Image
+                src={url}
+                alt={"Photo"}
+                w="100%"
+                h="auto"
+                rounded="md"
+                onClick={() =>
+                  masonryType === "form" ? null : handleToggleVisor(data)
+                }
+              />
+            </Tooltip>
+          </Box>
+        </GridItem>
+      ) : (
+        <Skeleton height={"sm"} />
+      )}
     </>
   );
 }
