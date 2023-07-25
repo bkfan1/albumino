192f37e576449ee9591af88d0443e80216a9c6eb
bkfan1 <jpf8296@gmail.com>
2023-07-25 18:30:45 -0400

    Add conditional rendering for showing empty state

--- src/components/ui/masonry/MasonryGrid.js
+++ src/components/ui/masonry/MasonryGrid.js
@@ -1,19 +1,44 @@
-import { Box, Grid } from "@chakra-ui/react";
+import {
+  Box,
+  Grid,
+  Heading,
+  Icon,
+  Image,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
 import MasonryGridItem from "./MasonryGridItem";
 import { useContext, useEffect, useState } from "react";
 import { MasonryGridContext } from "@/contexts/MasonryGridContext";
+import { AlbumPageContext } from "@/contexts/AlbumPageContext";
+import { MdOutlineAddPhotoAlternate } from "react-icons/md";
 
 export default function MasonryGrid({ masonryType }) {
-  const {masonryPhotos} = useContext(MasonryGridContext);
-  
+  const { inAlbumPage } = useContext(AlbumPageContext);
+
+  const { masonryPhotos } = useContext(MasonryGridContext);
+
   return (
     <>
-      <Box width={"100%"}>
-        <Grid
-          templateColumns="repeat(auto-fill, minmax(200px, 1fr))"
-          gap={4}
-          paddingBottom={4}
-        >
+      {inAlbumPage && masonryPhotos.length === 0 ? (
+        <VStack width={"100%"}>
+          <Image src="/empty_state_album.svg" alt="" />
+          <Heading size={"md"} fontWeight={"normal"}>
+            Album is empty
+          </Heading>
+          <Text maxW={"sm"} textAlign={"center"} fontWeight={"bold"}>
+            Add or upload photos to album using{" "}
+            <Icon as={MdOutlineAddPhotoAlternate} boxSize={6} mx={1} />
+            at the navigation bar.
+          </Text>
+        </VStack>
+      ) : (
+        <Box width={"100%"}>
+          <Grid
+            templateColumns="repeat(auto-fill, minmax(200px, 1fr))"
+            gap={4}
+            paddingBottom={4}
+          >
             {masonryPhotos.map((photo) => (
               <MasonryGridItem
                 key={photo.id}
@@ -21,8 +46,9 @@ export default function MasonryGrid({ masonryType }) {
                 masonryType={masonryType}
               />
             ))}
-        </Grid>
-      </Box>
+          </Grid>
+        </Box>
+      )}
     </>
   );
 }
