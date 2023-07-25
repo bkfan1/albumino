c0ec4445b0675be9dfb57de01ef1d190d2713ce0
bkfan1 <jpf8296@gmail.com>
2023-07-25 18:37:38 -0400

    Add new function to parse date strings to date ISO 8601

--- src/utils/date/index.js
+++ src/utils/date/index.js
@@ -1,6 +1,20 @@
+import moment from "moment";
+
 export const compareDates = (a, b) => {
-    const dateA = a.metadata.datetime ? new Date(a.metadata.datetime) : new Date(a.uploaded_at);
-    const dateB = b.metadata.datetime ? new Date(b.metadata.datetime) : new Date(b.uploaded_at);
-  
-    return dateB - dateA; // Orden descendente (dateB - dateA en lugar de dateA - dateB)
-  };
+  const dateA = a.metadata.datetime
+    ? new Date(a.metadata.datetime)
+    : new Date(a.uploaded_at);
+  const dateB = b.metadata.datetime
+    ? new Date(b.metadata.datetime)
+    : new Date(b.uploaded_at);
+
+  return dateB - dateA; // Descending order (dateB - dateA instead of dateA - dateB)
+};
+
+export const convertToISO8601 = (dateStr) => {
+  const format = "YYYY:MM:DD HH:mm:ss";
+  const momentDate = moment(dateStr, format);
+  const iso8601Date = momentDate.toISOString();
+
+  return iso8601Date;
+};
